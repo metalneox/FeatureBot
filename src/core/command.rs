1a5351f1a5bf1705a55f1f201e1f81b7f71cf1b1
MetalNeox <metalneox@gmail.com>
2023-03-11 11:50:17 +0100

    Async+Sync sinergy

--- src/core/command.rs
+++ src/core/command.rs
@@ -2,6 +2,11 @@ use std::{collections::HashMap};
 
 use super::features::{screenshot};
 
+use std::future::Future;
+use std::pin::Pin;
+
+type AsyncFn = Pin<Box<dyn Future<Output = ()> + Send>>;
+
 /// Commands
 
 #[derive(Debug,PartialEq)]
@@ -17,18 +22,22 @@ impl Cmds {
   
         //INFO: Due hashmap dove una con parametri e una senza quella senza ritornano semplice
         //stringhe
-        let mut features: HashMap<String,fn(String)-> String> = HashMap::new();
-        
-        features.insert("!rot13".to_string(), caduceo::crypto::ciphers::rot13);
 
-        //
+        // per funzioni non async
+        //let mut features_map: HashMap<String,fn(String)-> String> = HashMap::new();
 
-                                                //FIX: Questa funzione non verrà mai chiamat        
-        let mut fet: HashMap<String,String> = HashMap::new();
-        fet.insert("!info".to_string(),"Sono il mitico".to_string());
+        //per funzioni asyncrone
+        let mut features_async: HashMap<String, AsyncFn> = HashMap::new();
 
+        features_async.insert("!twitch".to_string(),Box::pin(async {screenshot("ciao".to_string());}));
+
+        //per funzioni sync
+        let mut features_sync: HashMap<String,fn(String)-> String> = HashMap::new();
+        features_sync.insert("!rot13".to_string(), caduceo::crypto::ciphers::rot13 as fn(String) -> String);
+
+        //println!("{:#?}",self);
     
-        if features.get(&self.cmd).is_some(){
+        if features_async.get(&self.cmd).is_some(){
             
             //TODO: Il controllo va bene ma se c'è una funzione senza parametro non viene gestito.
             if self.value.is_some() {
@@ -36,13 +45,16 @@ impl Cmds {
                 
                 if self.cmd == "!twitch" {
                     
+                    println!("{}",self.cmd);
                     let result = screenshot(self.value.unwrap()).await;
 
-                    return Ok(result);
+                    Ok(result)
 
                 }else{
-                    let result = features.get(&self.cmd).unwrap()(self.value.unwrap());
-                    return Ok(result);
+                    //let result = features_map.get(&self.cmd).unwrap()(self.value.unwrap());
+                    //return Ok(result);
+                    //
+                    Ok("prova".to_string())
  
                 }
 
@@ -56,10 +68,17 @@ impl Cmds {
 
             }
             
-            //Ok("Manca il valore da passare".to_string())
-
         }else{
-            Err("Funzionalità non trovata".to_string())
+
+            if features_sync.get(&self.cmd).is_some(){
+                let result = features_sync.get(&self.cmd).unwrap()(self.value.unwrap());
+                Ok(result)
+                
+            }else {
+
+                Err("Funzionalità non trovata".to_string())
+            }
+
         }
 
     }
