21ed789be00348aeedfc3c56c4a5ae4790c5eeae
MetalNeox <metalneox@gmail.com>
2024-05-04 20:20:49 +0200

    Add in alpha test ollama api access

--- src/core/command.rs
+++ src/core/command.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashMap};
+use std::collections::HashMap;
 
-use super::features::{screenshot,chatgpt};
+use super::features::{chatgpt, ollama, screenshot};
 
 use std::future::Future;
 use std::pin::Pin;
@@ -9,65 +9,59 @@ type AsyncFn = Pin<Box<dyn Future<Output = ()> + Send>>;
 
 /// Commands
 
-#[derive(Debug,PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Cmds {
-    pub cmd:String,
-    pub value:Option<String>,
+    pub cmd: String,
+    pub value: Option<String>,
 }
 
-impl Cmds {      
-    pub async fn run(self)-> Result<String,String>{
+impl Cmds {
+    pub async fn run(self) -> Result<String, String> {
         //per funzioni asyncrone
         let mut features_async: HashMap<String, AsyncFn> = HashMap::new();
 
-        features_async.insert("!twitch".to_string(),Box::pin(async {screenshot("ciao".to_string());}));
-		//FIX: Dovrebbe andare ma le api non andavano troppe richieste
-		let prova = chatgpt("prova".to_string()).await;
-		println!("->{:#?}",prova);
-		
-		//println!("{:#?}",prova);
-        //per funzioni sync
-        let mut features_sync: HashMap<String,fn(String)-> String> = HashMap::new();
-        features_sync.insert("!rot13".to_string(), caduceo::crypto::ciphers::rot13 as fn(String) -> String);
+        features_async.insert(
+            "!twitch".to_string(),
+            Box::pin(async {
+                screenshot("ciao".to_string());
+            }),
+        );
+        //FIX: Dovrebbe andare ma le api non andavano troppe richieste
+        let prova = ollama("Cosa sei tu?".to_string()).await;
+        println!("->{:#?}", prova);
 
-    
-        if features_async.get(&self.cmd).is_some(){
-            
+        //println!("{:#?}",prova);
+        //per funzioni sync
+        let mut features_sync: HashMap<String, fn(String) -> String> = HashMap::new();
+        features_sync.insert(
+            "!rot13".to_string(),
+            caduceo::crypto::ciphers::rot13 as fn(String) -> String,
+        );
+
+        if features_async.get(&self.cmd).is_some() {
             //TODO: Il controllo va bene ma se c'è una funzione senza parametro non viene gestito.
             if self.value.is_some() {
                 //FIX: Controllare option
-                
+
                 if self.cmd == "!twitch" {
-                
                     let result = screenshot(self.value.unwrap()).await;
 
                     Ok(result)
-
-                }else{
-                    Ok("prova".to_string()) 
+                } else {
+                    Ok("prova".to_string())
                 }
-           }else{
-
+            } else {
                 let result = "to".to_string();
                 Ok(result)
-
             }
-            
-        }else{
-
-            if features_sync.get(&self.cmd).is_some(){
+        } else {
+            if features_sync.get(&self.cmd).is_some() {
                 let result = features_sync.get(&self.cmd).unwrap()(self.value.unwrap());
                 Ok(result)
-                
-            }else {
-
+            } else {
                 Err("Funzionalità non trovata".to_string())
             }
-
         }
-
     }
-
 }
-
 
--- src/core/features.rs
+++ src/core/features.rs
@@ -1,10 +1,10 @@
-use reqwest::{Client,header,Error};
+use reqwest::{header, Client, Error};
 use serde_json::{json, Value};
 use std::env;
 //use select::document::Document;
 //use select::predicate::{Attr, Name, Predicate};
 
-async fn get_img(url: String) -> Result<String,String> {
+async fn get_img(url: String) -> Result<String, String> {
     let client = Client::new();
     //let resp = client.get(url).send().await?.text().await?;
 
@@ -12,29 +12,19 @@ async fn get_img(url: String) -> Result<String,String> {
 
     let resp = client.get(url).send().await.unwrap().status();
 
-    if resp.is_success(){
+    if resp.is_success() {
         return Ok(url_img);
-    }else{
+    } else {
         let custom_url = "https://demofree.sirv.com/nope-not-here.jpg".to_string();
         return Err(custom_url);
     }
-     
-    //let document = Document::from_read(resp.as_bytes()).unwrap();
-
-    //for node in document.find(Attr("class", "preview").descendant(Name("img"))) {
-    //    let src = node.attr("src").unwrap();
-    //    return Ok(src.to_string());
-    //}
-
-    //Ok("".to_string())
 }
 
-pub async fn chatgpt(stringa: String)-> Result<String, Error>{
+pub async fn chatgpt(stringa: String) -> Result<String, Error> {
     let client2 = Client::new();
 
     //let url_img = url.clone();
 
-    
     let json_body = json!({
         "model": "gpt-3.5-turbo",
         "messages": [
@@ -49,92 +39,63 @@ pub async fn chatgpt(stringa: String)-> Result<String, Error>{
         ]
     });
 
-	//let api_key = "";
-	let api_key = env::var("OPENIA").unwrap_or(String::from(""));
-	
+    //let api_key = "";
+    let api_key = env::var("OPENIA").unwrap_or(String::from(""));
+
     let resp = client2
         .post("https://api.openai.com/v1/chat/completions")
         .header(header::AUTHORIZATION, format!("Bearer {}", api_key))
         .json(&json_body)
         .send()
-		.await?
-		.text()
-		.await;
-		//.status();
-		//.await;
-	//println!("->{:#?}",resp);
-	//let v: Value = serde_json::from_str(resp);
-	//resp.json().await.expect("REASON")
-	//v
-	resp
-
-/*
-curl https://api.openai.com/v1/chat/completions   -H "Content-Type: application/json"   -H "Authorization: Bearer $OPENAI_API_KEY"   -d '{
-    "model": "gpt-3.5-turbo",
-    "messages": [
-      {
-        "role": "system",
-        "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."
-      },
-      {
-        "role": "user",
-        "content": "Compose a poem that explains the concept of recursion in programming."
-      }
-    ]
-  }'
-*/
-
-    /*
-    if resp.status().is_success() {
-        // Leggi la risposta come JSON
-        let response_json: Value = response.json().await?;
-        println!("Risposta JSON:\n{}", response_json);
-    } else {
-        println!("La richiesta non è stata eseguita correttamente. Stato: {}", response.status());
-    }*/
-
-
-    /*
-    if resp.unwrap().is_success(){
-        //idea
-        //return Ok(resp.body);
-        let resp_json: Value = resp.json().await;
-        println!("Risposta JSON:\n{}", resp_json);
-
-        //return Ok("ciaone".to_string());
-    }else{
-        //return Err("Errore imprevisto".to_string());
-        //
-        println!("La richiesta non è stata eseguita correttamente. Stato: {}", resp.status());
-    }
-    */
-
+        .await?
+        .text()
+        .await;
+    resp
 }
 
+pub async fn ollama(stringa: String) -> Result<String, Error> {
+    let client2 = Client::new();
 
+    //let url_img = url.clone();
 
+    let json_body = json!({
+        "model": "phi3",
+        "prompt": stringa,
+        "format": "json",
+        "stream": false
+    });
 
-pub async fn screenshot(streamer:String) -> String{
+    //let api_key = env::var("OLLAMA").unwrap_or(String::from(""));
+
+    let resp = client2
+        .post("http://localhost:11434/api/generate")
+        //.header(header::AUTHORIZATION, format!("Bearer {}", api_key))
+        .json(&json_body)
+        .send()
+        .await?
+        .text()
+        .await;
+    resp
+}
+
+pub async fn screenshot(streamer: String) -> String {
     let base = "https://static-cdn.jtvnw.net/previews-ttv/".to_string();
     //lowercase?
-    let url = format!("{}live_user_{}-440x248.jpg",base,streamer.to_lowercase());
+    let url = format!("{}live_user_{}-440x248.jpg", base, streamer.to_lowercase());
 
-    println!("{}",url);
+    println!("{}", url);
     let result = get_img(url).await;
 
-    if result.is_ok(){ 
-
+    if result.is_ok() {
         let temp = result.unwrap();
 
-        if temp != "".to_string(){
-           return temp; 
+        if temp != "".to_string() {
+            return temp;
         }
 
         return "Streamer non trovato".to_string();
     }
 
     "".to_string()
-
 }
-
 
